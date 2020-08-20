9044c44f150e3faee223906e916e7a8647aba100
blakerom <blakeromero001@gmail.com>
2020-08-19 20:25:57 -0700

    updating past few hours of changes

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 'use strict';
 
+require('dotenv').config();
+
 const server = require('./lib/server.js');
 
-server.start(3002);
+const mongoose = require('mongoose');
+
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+}
+mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
+
+server.start(3000);
