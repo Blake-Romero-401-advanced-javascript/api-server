d80ac3baa0fb3c1b5f14ff23bd0339f9f36eacfb
blakerom <blakeromero001@gmail.com>
2020-08-20 22:16:11 -0700

    making more modular updates finished route page

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ const mongooseOptions = {
   useCreateIndex: true,
   useUnifiedTopology: true,
 }
-mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
+mongoose.connect(process.env.MONGOD_URI, mongooseOptions);
 
 server.start(3000);
